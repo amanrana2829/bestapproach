aa5fbd688f855ec4f1a4be60620b2b60806d5565
amanrana2829 <amanofficial131@gmail.com>
2025-06-22 06:11:33 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,12 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.jsx',
-            refresh: true,
-        }),
-        react(),
-    ],
-     server: {
-        host: '127.0.0.1',
-       port: 5172,
-         strictPort: true
-    },
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.jsx'],
+      refresh: true,
+    }),
+  ],
+  base: '/build/',
 });
