5ce0809af1e0cd229b2bbf883301f2b4ad60a737
amanrana2829 <amanofficial131@gmail.com>
2025-06-22 14:48:36 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,15 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
   plugins: [
     laravel({
-      input: ['resources/css/app.css', 'resources/js/app.jsx'],
+      input: ['resources/js/app.jsx'], // your entry points
       refresh: true,
     }),
   ],
   build: {
-    outDir: 'public/build', // ✅ Ensure Vite outputs to public/build
-    emptyOutDir: true,
+    outDir: 'public/build', // very important
+    manifest: true,
+    rollupOptions: {
+      input: 'resources/js/app.jsx',
+    },
   },
-  base: '/build/',
 });
